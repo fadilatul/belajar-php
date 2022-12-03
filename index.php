9ebfe0086d3f540ee1a6d92693ac1c832179b71e
Fadilatul Rohmania <fadilatulfadilatul95@gmail.com>
2022-12-03 12:48:53 +0700

    menambahkan file php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello World!<br>";
+
+$nama = "Fadilatul Rohmania";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Faidatul Aulia";
+$umurAdik = 14;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
